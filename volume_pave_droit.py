e3d29fb3c00d4330935c8af9b51c10dde1509b19
Elhendro <ahbcool06@gmail.com>
2024-10-01 12:22:44 +0200

    Add files via upload
    Programme qui calcule le volume d’un parallélépipède rectangle dont sont fournis
    au départ la largeur, la hauteur et la profondeur.

---
+++ volume_pave_droit.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  1 08:41:12 2024
+
+@author: Hendrick A. BALLEY
+"""
+
+#Écrivez un programme qui calcule le volume d’un parallélépipède rectangle 
+#dont sont fournis au départ la largeur, la hauteur et la profondeur.
+
+L = float(input("Entrez la longueur de votre pavé droit: "))
+l = float(input("Entrez la largeur de votre pavé droit: "))
+h = float(input("Entrez la profondeur de votre pavé droit: "))
+
+print("Le volume de votre pavé droit est égal à: ",L*l*h)
